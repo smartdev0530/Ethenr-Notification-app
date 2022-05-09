4d6602c6c21a801aeddb40163fd11b24e6807ad6
david <smdev00001@gmail.com>
2022-05-09 14:02:50 -0700

    change config.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cron = require("node-cron");
 const notifier = require("node-notifier");
 const BuildTransactionChecker = require("./transactionChecker");
 const { WALLET_ADDRESS } = require("./config");
-const { ALRM_TIME } = require("./config");
+const { ALARM_TIME } = require("./config");
 const { LIMIT_ETH_AMOUNT } = require("./config");
 const CreateClient = require("./ethClient");
 const { web3http, web3 } = CreateClient(Web3);
@@ -12,7 +12,7 @@ const watchTransactions = BuildTransactionChecker({ web3, web3http });
 watchTransactions();
 
 var task = cron.schedule(
-  `50 10 * * *`,
+  `0 ${ALARM_TIME} * * *`,
   async () => {
     try {
       web3.eth.getBalance(WALLET_ADDRESS).then((result) => {
